ee45303e908602c1cae3cd4e84ef620fc3ea2ca3
Tipper01 <kyletippets31@gmail.com>
2016-09-23 08:10:38 -0600

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$("#aboutmeinfo").hide();
+	/* we need to create this next time */
+
+	$("#aboutmebutton").click(function() {
+
+		$("#aboutmeinfo").slideToggle(500);
+
+	});
+
+});
+
+
+
+
